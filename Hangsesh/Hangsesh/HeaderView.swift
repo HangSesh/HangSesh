0af28bf2b5c4c193ed0aaa60c657db5fbd66e92b
abhimalamkar <abhijeetmalamkar@gmail.com>
2017-02-08 02:43:52 -0500

    HeaderView Class Added
    Added some fiction in new controller for header of the posts

---
+++ Hangsesh/Hangsesh/HeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  HeaderView.swift
+//  app
+//
+//  Created by Abhijeet Malamkar on 2/8/17.
+//  Copyright © 2017 Abhijeet Malamkar. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewCell {
+    
+    
+    @IBOutlet weak var HeaderLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
--- Hangsesh/Hangsesh/ViewController.swift
+++ Hangsesh/Hangsesh/ViewController.swift
@@ -1,25 +1,102 @@
 //
 //  ViewController.swift
-//  Hangsesh
+//  app
 //
-//  Created by Abhijeet Malamkar on 2/7/17.
-//  Copyright © 2017 Hangsesh. All rights reserved.
+//  Created by Abhijeet Malamkar on 2/5/17.
+//  Copyright © 2017 Abhijeet Malamkar. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+struct postSruct {
+    var name:String!
+}
 
+var heightOfHeader:CGFloat = 44
+
+class TableViewController: UITableViewController {
+    
+    var arrayOfPosts = [postSruct]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        f()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        arrayOfPosts = [postSruct.init(name: "Abhijeet"),postSruct.init(name: "Abhijeet"),postSruct.init(name: "Abhijeet"),postSruct.init(name: "Abhijeet")]
+        
+        
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return arrayOfPosts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return heightOfHeader
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)?.first as! HeaderView
+        
+        headerView.HeaderLabel.text = arrayOfPosts[section].name
+        
+        return headerView
+    }
+    
+    func f()
+    {
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: "https://api.topcoder.com/v2/develop/challenges?pageSize=2")!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!.localizedDescription)
+                
+            } else {
+                
+                do {
+                    
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    {
+                        
+                        //Implement your logic
+                        print(json)
+                        
+                    }
+                    
+                } catch {
+                    
+                    print("error in JSONSerialization")
+                    
+                }
+                
+                
+            }
+            
+        })
+        task.resume()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
